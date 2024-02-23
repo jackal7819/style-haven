c06cbd35e98261dfe62e2b6a036926940ba9c285
Viktor Filippov <jackal3166@gmail.com>
2024-02-23 23:35:10 +0200

    Add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	head: {
+		title: 'Style Haven',
+		link: [{ rel: 'icon', type: 'image/svg+xml', href: '/pinia.svg' }],
+	},
 	devtools: { enabled: true },
 	css: ['/main.css'],
 	postcss: {
