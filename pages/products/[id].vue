65f457a7aecf279fa3644bc04e68b9391c78bf94
Viktor Filippov <jackal3166@gmail.com>
2024-02-24 00:10:30 +0200

    Create products

---
+++ pages/products/[id].vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+	<div class='p-[5%]'>
+		<h1 class="mb-8 text-3xl font-bold">Product details for {{ $route.params.id }}</h1>
+		<p>
+			We take pride in offering you not just clothes, but a true stylish
+			lifestyle. Our clothing is crafted with care for details and quality
+			materials, so that every customer feels confident and stylish in any
+			situation.
+		</p>
+	</div>
+</template>
