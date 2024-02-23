ca915507bc39a7c872f1297d693a535a88d5a4a1
Viktor Filippov <jackal3166@gmail.com>
2024-02-23 23:56:14 +0200

    Create About

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="p-[5%]">
+		<h1 class="mb-8 text-3xl font-bold">About</h1>
+		<p>
+			Ready to refresh your wardrobe and add fresh accents to your look?
+			Our stylish clothing store invites you on an exciting journey
+			through the world of fashion!
+		</p>
+		<p>
+			Our collections are inspired by the latest trends and created to
+			emphasize your individuality and express your uniqueness. From
+			luxurious fabrics to bold prints, from classic shapes to bold
+			decisions - we have everything you need to make your style bright
+			and memorable.
+		</p>
+		<p>
+			Allow yourself to transform and discover new horizons of style with
+			our collection. Choose quality, choose style, choose us!
+		</p>
+	</div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+	<div class="p-[5%]">
+		<h1 class="mb-8 text-3xl font-bold">Style Haven</h1>
+		<p>
+			Let's dive into the world of fashion, where every detail matters,
+			and style is a way to express your uniqueness. Our clothing
+			collections are the perfect blend of elegance, comfort, and the
+			latest fashion trends.
+		</p>
+		<p>
+			We take pride in offering you not just clothes, but a true stylish
+			lifestyle. Our clothing is crafted with care for details and quality
+			materials, so that every customer feels confident and stylish in any
+			situation.
+		</p>
+		<p>
+			From classic basics to bold and modern trends - we have everything
+			to highlight your individuality and elevate your style to a new
+			level. Welcome to the world of stylish clothing, where everyone can
+			become a true style icon!
+		</p>
+	</div>
 </template>
